3b2f7489317d112d9443f54b65d73d999dea8535
James <jamesdiffey8@gmail.com>
2024-01-14 10:56:04 +0000

    scoring (locally)

--- main.js
+++ main.js
@@ -1,25 +1,42 @@
 console.log("main.js file running")
 
+/* EXAMPLE QUERY
+ INSERT INTO bananaandivyleaderboard (username, time_in_seconds, date)
+ VALUES ('James', '20', '2024-01-01')
+
+ */
+
 // query selectors
 const bananas = document.querySelectorAll(".banana")
 const poisonousplants = document.querySelectorAll(".poison")
 const grid = document.querySelector(".grid")
 const bananacount = document.querySelector("#bananacount")
 const bananacountnoun = document.querySelector("#bananacountnoun")
-const start = document.querySelector(".start")
-
+const startblock = document.querySelector(".start")
 const finish = document.querySelector(".finish")
+const timer = document.querySelector("#timer")
 
 // event listeners
 // -- prevent right clicking in grid
 grid.addEventListener("contextmenu", () => {event.preventDefault()})
 
+let clock=0;
 let firstPlay = true;
-start.addEventListener("mouseover", () => {
-    resetGame();
-    firstPlay = false;
 
-})
+let timerInterval; // Declare a variable to store the interval
+
+startblock.addEventListener("mouseover", () => {
+  resetGame();
+  firstPlay = false;
+  clock = 0;
+  clearInterval(timerInterval); // Clear previous interval if exists
+  timerInterval = setInterval(() => {
+    clock++;
+    timer.textContent = clock;
+  }, 1000);
+    gameOngoing = true;
+  }
+);
 finish.addEventListener("mouseover", () => {if (score > 7) {
     alert("You win!")
     resetGame();
@@ -59,7 +76,8 @@ bananas.forEach(banana => {
 
 
     });
-  
+
+
   function resetGame() {
     score = 0;
     bananas.forEach(banana => {
